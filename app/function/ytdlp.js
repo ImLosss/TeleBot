d223321bcc94bda5221565e84dba3441b60e3df4
ImLosss <pahriansyahryan@gmail.com>
2025-06-01 15:26:49 +0800

    update

--- app/function/ytdlp.js
+++ app/function/ytdlp.js
@@ -64,6 +64,7 @@ async function ytdlp(bot, msg, value, config) {
                 }
 
                 let subid = Math.random().toString(36).substr(2, 5);
+                if(tempData[id] === undefined) tempData[id] = {};
                 tempData[id][subid] = {title: info.title, url: value, format_id: fmt.format_id, acodec: fmt.acodec == 'none' ? false : true, ext: fmt.ext, user_id: msg.from.id, id: id}; // Simpan URL sementara
                 return {
                     text: `${fmt.ext} | ${fmt.format_note || fmt.resolution || ''}${sizeMB}`,
