97f628bb5151133d8ac83a29622ef1dc53463a30
ImLosss <pahriansyahryan@gmail.com>
2025-06-21 03:08:42 +0800

    update

--- app/function/iqiyi.js
+++ app/function/iqiyi.js
@@ -40,7 +40,7 @@ async function downloadIqiyi(bot, msg, value, config) {
     const outputTemplate = path.join(outputDir, `${id}.%(ext)s`);
     cmd = `yt-dlp -f ${format_id} --remux-video ${ext} --write-sub --sub-langs id --sub-format srt --embed-subs -o "${outputTemplate}" "${url}" --no-warnings --no-call-home --no-check-certificate --ffmpeg-location /usr/bin/ffmpeg --cookies-from-browser firefox`;
 
-    if (hardsub) cmd = `yt-dlp -f ${format_id} --remux-video ${ext} --write-sub --sub-langs id --sub-format srt -o "${outputTemplate}" "${url}" --no-warnings --no-call-home --no-check-certificate --ffmpeg-location /usr/bin/ffmpeg --cookies-from-browser firefox && node merged.js "downloads/${id}.id.srt" && ffmpeg -i "downloads/${id}.${ext}" -crf "26" -vf "subtitles=downloads/${id}.id.srt:force_style='FontName=Arial,FontSize=${fontSize},PrimaryColour=&HFFFFFF&,Outline=${outline},MarginV=${y},Bold=1',drawtext=text='DongWorld':font=Verdana:fontsize=${wmSize}:fontcolor=white@0.5:x=15:y=15" -c:a copy "downloads/${id}_hardsub.${ext}"`;
+    if (hardsub) cmd = `yt-dlp -f ${format_id} --remux-video ${ext} --write-sub --sub-langs id --sub-format srt -o "${outputTemplate}" "${url}" --no-warnings --no-call-home --no-check-certificate --ffmpeg-location /usr/bin/ffmpeg --cookies-from-browser firefox && node merged.js "downloads/${id}.id.srt" && ffmpeg -i "downloads/${id}.${ext}" -crf "26" -vf "subtitles=downloads/${id}.id.srt:force_style='FontName=Arial,FontSize=${fontSize},PrimaryColour=&HFFFFFF&,Outline=${outline},MarginV=${y},Bold=1',drawtext=text='DongWorld':font=Verdana:fontsize=${wmSize}:fontcolor=white@0.5:x=15:y=15" -c:a copy "downloads/${id}_hardsub.${ext}" && ffmpeg -ss 1 -i "downloads/${id}_hardsub.${ext}" -frames:v 1 -q:v 2 "downloads/ss1.jpg" && ffmpeg -ss 300 -i "downloads/${id}_hardsub.${ext}" -frames:v 1 -q:v 2 "downloads/ss2.jpg" && ffmpeg -ss 600 -i "downloads/${id}_hardsub.${ext}" -frames:v 1 -q:v 2 "downloads/ss3.jpg"`;
 
     const loadingMsg = await bot.sendMessage(msg.chat.id, 'Mulai mendownload...');
 
@@ -79,6 +79,13 @@ async function downloadIqiyi(bot, msg, value, config) {
                         }
                         const linkData = await generatePublicURL(fileId);
                         if (linkData && linkData.webViewLink) {
+                            const screenshots = [
+                                { type: 'photo', media: { source: 'downloads/ss1.jpg' } },
+                                { type: 'photo', media: { source: 'downloads/ss2.jpg' } },
+                                { type: 'photo', media: { source: 'downloads/ss3.jpg' } }
+                            ];
+
+                            await bot.sendMediaGroup(msg.chat.id, screenshots);
                             bot.sendMessage(msg.chat.id, `File berhasil diupload ke Google Drive\n\n*Durasi:* ${durationStr}\n*Filesize:* ${Math.floor(stats.size / 1048576)}mb\n\nFile akan dihapus dalam 1 jam kedepan`, {
                                 parse_mode: 'Markdown',
                                 reply_markup: {
